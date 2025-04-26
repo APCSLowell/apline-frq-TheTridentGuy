72a656c61253a141d30195d847307e49f7de8b07
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-04-25 23:48:27 -0700

    its an improvement

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,20 @@
 public class APLine
 {
   /* your code here */
-  
+  private int a;
+  private int b;
+  private int c;
+  public APLine(int A, int B, int C)
+  {
+    a = A;
+    b = B;
+    c = C;
+  }
+  public double getSlope(){
+    return -(double)a/b;
+  }
+  public boolean isOnLine(int x, int y)
+  {
+    return a*x + b*y + c == 0;
+  }
 }
